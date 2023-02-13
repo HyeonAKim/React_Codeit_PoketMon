13a592c00ce3d9c8bd04956342bea70cc3f95d0f
hyeona kim <foren4012@gmail.com>
2023-02-13 20:25:42 +0900

    00.init environment

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import ReactDOM from "react-dom/client";
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
